6ebe5c3c551aa57dea653640fb8fad191b8cafaf
flyingbest <taeyoon4715@gmail.com>
2015-09-07 20:15:50 +0900

    코딩 중

---
+++ COINS/COINS.cpp
@@ -0,0 +1,26 @@
+/*
+	 COINS
+
+	 @author Taeyoon Moon
+	 @dated 2015. 09. 07
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+	int T, M, C;
+
+	cin >> T;
+	for(int i=0; i<T; i++){
+		cin >> M >> C;
+		fot(int j=0; j<C; j++){
+			int set[j] = 0;
+			cin >> set[j];
+		}
+		M / set[C-1];
+		
+
+
+	}
+
