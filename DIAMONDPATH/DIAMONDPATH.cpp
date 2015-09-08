e1d02fe25a6a6aaafbd20e4faac2546e6ebbdd05
flyingbest <taeyoon4715@gmail.com>
2015-09-08 13:38:19 +0900

    sort of problem

---
+++ DIAMONDPATH/DIAMONDPATH.cpp
@@ -0,0 +1,28 @@
+/*
+	 DIAMONDPATH
+
+	 @author Taeyoon Moon
+	 @dated 2015. 09. 08
+
+	 using Dynamic Programming(DP) - 동적 계획법
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+	int C, N;
+
+	cin >> C;
+	for(int i=0; i<C; i++){
+		cin >> N;
+		for(int j=0; j<(2*N-1); j++){
+			
+		}
+		
+	}
+
+	return 0;
+}
+	
---
+++ DIAMONDPATH/README.md
@@ -0,0 +1,3 @@
+# Dynamic Programming(DP)
+	- In mathematics, compter science, economics, and bioinformatics, dynamic programming is a method for solving a complex problem by breaking it down into a collection of simpler subproblems.
+
